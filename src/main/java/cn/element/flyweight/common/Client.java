5ba6b841f481838b4d8be8ce38ca300dea614960
Dell <1529146935@qq.com>
2022-01-17 16:42:58 +0800

    2022-01-17 17:00:00 FLYWEIGHT

---
+++ src/main/java/cn/element/flyweight/common/Client.java
@@ -0,0 +1,15 @@
+package cn.element.flyweight.common;
+
+public class Client {
+
+    public static void main(String[] args) {
+        IFlyweight flyweight1 = FlyweightFactory.getFlyweight("aa");
+        IFlyweight flyweight2 = FlyweightFactory.getFlyweight("bb");
+
+        flyweight1.operation("a");
+        flyweight2.operation("b");
+
+        IFlyweight flyweight3 = FlyweightFactory.getFlyweight("aa");
+        flyweight3.operation("a");
+    }
+}
---
+++ src/main/java/cn/element/flyweight/ticket/ITicket.java
@@ -0,0 +1,6 @@
+package cn.element.flyweight.ticket;
+
+public interface ITicket {
+
+    void showInfo(String bunk);
+}
